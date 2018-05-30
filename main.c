c05e832d1f35ccb04959189c2e007f342b2b6a57
Vinicio Valbuena <formatcomvinicio@gmail.com>
2018-05-30 14:47:02 -0500

    commit init

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int randomInt(int max, int min){
+	return rand() % (min-max+1)+max;
+}
+
+char* randomString(int size){
+	char *string = malloc(sizeof(char)*size);
+	for (int i=0; i<size; i++) string[i] = randomInt(65, 90);
+	return string;
+}
+
+int main(void) {
+	srand(time(NULL));
+	for (int i = 0; i < 5; i++) printf("%d: %d\n", i+1, randomInt(10, 30));
+
+	printf("rand: %s\n", randomString(64));
+	return 0;
+}
